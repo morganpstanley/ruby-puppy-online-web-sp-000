f8f578836cd978f99c885c79aebc54ed1e25649a
Morgan Stanley <morganpstanley@gmail.com>
2019-07-11 06:23:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+
+require 'pry'
+
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    return @@all
+  end
+
+  def name
+    @name
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+end
